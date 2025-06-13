495b50e7a6fe4f9b8d5cda2a65c85866eca8d803
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-06-13 21:13:29 +0300

    Добавлена страница календаря

--- ToDo.Frontend/Pages/TaskItems/TaskFormViewModel.cs
+++ ToDo.Frontend/Pages/TaskItems/TaskFormViewModel.cs
@@ -27,8 +27,32 @@ namespace ToDo.Frontend.Pages.TaskItems
         [Required]
         public TimeSpan? EndTime { get; set; }
 
+        private int _durationBlocks = 4; // 4×15м = 1ч
         [Required]
-        public int DurationHours { get; set; }
+        public int DurationBlocks
+        {
+            get => _durationBlocks;
+            set
+            {
+                if (value == _durationBlocks) return;
+                _durationBlocks = value;
+                UpdateEndByDuration();
+            }
+        }
+
+        // человекочитаемая подпись
+        public string DurationLabel
+        {
+            get
+            {
+                var totalMin = _durationBlocks * 15;
+                var h = totalMin / 60;
+                var m = totalMin % 60;
+                return h > 0
+                    ? (m > 0 ? $"{h}ч {m} мин" : $"{h}ч")
+                    : $"{m} мин";
+            }
+        }
 
         [MaxLength(20)]
         public string Color { get; set; } = "#2196F3";
@@ -40,6 +64,16 @@ namespace ToDo.Frontend.Pages.TaskItems
         // если нужна рекурсия
         public bool IsRecurring { get; set; }
         public string? RecurrenceRule { get; set; }
+        private void UpdateEndByDuration()
+        {
+            if (StartDate.HasValue && StartTime.HasValue)
+            {
+                var start = StartDate.Value.Date + StartTime.Value;
+                var end = start.AddMinutes(_durationBlocks * 15);
+                EndDate = end.Date;
+                EndTime = end.TimeOfDay;
+            }
+        }
     }
 
 }
--- ToDo.Frontend/Program.cs
+++ ToDo.Frontend/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Syncfusion.Blazor;
 using ToDo.Frontend;
 using ToDo.Frontend.Services.Auth;
 using ToDo.Frontend.Services.TaskItems;
@@ -12,6 +13,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddMudServices();
 
 builder.Services.AddBlazoredLocalStorage();
