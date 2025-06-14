8f752c190093a82510e35d53cca1c49dc4849e24
Daniil Varezkin <daniil.varezkin@gmail.com>
2025-06-14 03:26:27 +0300

    Доработки

---
+++ ToDo.Frontend/Common/Extensions/TaskPriorityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MudBlazor;
+using ToDo.Shared.Enums;
+
+namespace ToDo.Frontend.Common.Extensions
+{
+    public static class TaskPriorityExtensions
+    {
+        public static Color GetPriorityMudColor(this TaskPriority pri) => pri switch
+        {
+            TaskPriority.High => Color.Error,
+            TaskPriority.Medium => Color.Primary,
+            TaskPriority.Low => Color.Secondary,
+            _ => Color.Default
+        };
+    }
+}
---
+++ ToDo.Frontend/Common/Extensions/UserTaskStatusExtension.cs
@@ -0,0 +1,16 @@
+﻿using MudBlazor;
+using ToDo.Shared.Enums;
+
+namespace ToDo.Frontend.Common.Extensions
+{
+    public static class UserTaskStatusExtension
+    {
+        public static Color GetStatusMudColor(this UserTaskStatus status) => status switch
+        {
+            UserTaskStatus.Done => Color.Success,
+            UserTaskStatus.InProgress => Color.Info,
+            UserTaskStatus.Todo => Color.Warning,
+            _ => Color.Default
+        };
+    }
+}
